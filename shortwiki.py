3711335618bf45427a9cad4408f66ccbbc217d4d
Jeff Lindsay <progrium@gmail.com>
2009-12-14 03:55:23 -0800

    better refactoring into wiki module

--- shortwiki.py
+++ shortwiki.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 
-from __future__ import with_statement
 import sys, time
 import urllib
 
 import sms
 import wiki
 
+ERROR_MARK = "###"
+
 def get_requests():
     requests = []
     for msg in sms.receive():
-        requests.append(dict(
+        if not msg['text'].startswith(ERROR_MARK):
+            requests.append(dict(
                 src=msg['phoneNumber'], 
                 txt=msg['text'], 
                 t=int(time.mktime(msg['startTime']))-28800))
@@ -20,53 +22,6 @@ def moses(requests):
     reads = [r for r in requests if r['txt'].find(' ') == -1]
     writes = [r for r in requests if r['txt'].find(' ') != -1]
     return reads, writes
-
-def do_write(write, wiki):
-    src = write['src']
-    t = write['t']
-    txt = write['txt']
-
-    idx = txt.find(' ')
-    page = txt[:idx]
-    content = txt[idx+1:]
-    wiki[page] = {
-        'page': page,
-        'content': content,
-        'author': src,
-        'mtime': int(t)}
-    print "Write on page '%s' from %s." % (page, src)
-
-def do_read(read, wiki):
-    src = read['src']
-    t = read['t']
-    txt = read['txt']
-    page = txt
-
-    if txt in wiki:
-        entry = wiki[page]
-        content = entry['content']
-        author = entry['author']
-        sms.send(src, "%s %s/%s" % (content, author, time_ago(entry['mtime'])))
-        print "Read on page '%s' from %s." % (txt, src)
-    else:
-        print "Read on unknown page '%s' from %s." % (txt, src)
-        sms.send(src, '### Not found')
-
-def time_ago(from_time):
-    seconds = int(time.time()) - from_time
-    if seconds < 60:
-        return "%ds" % int(seconds)
-    if seconds < 3600:
-        return "%dm" % int(seconds/60)
-    if seconds < 86400:
-        return "%dh" % int(seconds/3600)
-    if seconds < 2629743.83:
-        return "%dd" % int(seconds/86400)
-    if seconds < 31556929:
-        return "%dmo" % int(seconds/2629743.83)
-    else:
-        return "%dyr" % int(seconds/31556929)
-
 
 def main(args):
     wiki.init()
@@ -82,12 +37,12 @@ def main(args):
 
         # process writes before reads so readers get the latest data
         for write in writes:
-            do_write(write, wikidata)
+            wiki.write(write, wikidata)
             
         wiki.dump(wikidata)
 
         for read in reads:
-            do_read(read, wikidata)
+            wiki.read(read, wikidata)
 
         time.sleep(3)
 
--- wiki.py
+++ wiki.py
@@ -1,4 +1,9 @@
+from __future__ import with_statement
 import simplejson
+import time
+
+from shortwiki import ERROR_MARK
+import sms
 
 WIKI_FILE = 'wiki.json'
 
@@ -17,3 +22,49 @@ def dump(wiki):
     with open(WIKI_FILE, 'w') as f:
         simplejson.dump(wiki, f)
         f.write("\n")
+
+def write(write, wiki):
+    src = write['src']
+    t = write['t']
+    txt = write['txt']
+
+    idx = txt.find(' ')
+    page = txt[:idx]
+    content = txt[idx+1:]
+    wiki[page] = {
+        'page': page,
+        'content': content,
+        'author': src,
+        'mtime': int(t)}
+    print "Write on page '%s' from %s." % (page, src)
+
+def read(read, wiki):
+    src = read['src']
+    t = read['t']
+    txt = read['txt']
+    page = txt
+
+    if txt in wiki:
+        entry = wiki[page]
+        content = entry['content']
+        author = entry['author']
+        sms.send(src, "%s %s/%s" % (content, author, time_ago(entry['mtime'])))
+        print "Read on page '%s' from %s." % (txt, src)
+    else:
+        print "Read on unknown page '%s' from %s." % (txt, src)
+        sms.send(src, '%s Not found' % ERROR_MARK)
+
+def time_ago(from_time):
+    seconds = int(time.time()) - from_time
+    if seconds < 60:
+        return "%ds" % int(seconds)
+    if seconds < 3600:
+        return "%dm" % int(seconds/60)
+    if seconds < 86400:
+        return "%dh" % int(seconds/3600)
+    if seconds < 2629743.83:
+        return "%dd" % int(seconds/86400)
+    if seconds < 31556929:
+        return "%dmo" % int(seconds/2629743.83)
+    else:
+        return "%dyr" % int(seconds/31556929)
